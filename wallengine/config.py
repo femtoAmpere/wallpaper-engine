89a778e1e62ac614c34549221474180e961c5e8d
femtoAmpere <3886111+femtoAmpere@users.noreply.github.com>
2020-11-07 16:14:24 +0100

    Update config.py
    readability update

--- wallengine/config.py
+++ wallengine/config.py
@@ -1,9 +1,9 @@
-slideshow_minutes = 10
-slideshow_screens = 3
+slideshow_minutes = 10  # minutes until wallpaper swaps
+slideshow_screens = 3  # number of screens/monitors you are using
 directory = "wallpapers"
 tags = [
-#    "width:>2560",  # ~ One of the resolutions.
-    "height:>1440",
+    #"width:>2560",  # use width if you select "fill" in wallpaper settings
+    "height:>1440",  # use height if you select "fit" in wallpaper settings
     "-animated",
     "big_breasts",
     "order:score",
